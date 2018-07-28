d7dc7cd5d7cd047850af7ad295136d1ab18c1b56
Sebastian Feldmann <sebastian.feldmann.hb@web.de>
2018-07-28 16:12:55 -0400

    Reworking egghunter

--- Assignment3/EggHunter.asm
+++ Assignment3/EggHunter.asm
@@ -5,36 +5,11 @@ the_egg equ 'AAAA'
 global _start:
 section .text
 _start:
-
-    ; Get current top of the stack
-    push rsp
-    pop rdi
-
+    add rsp, 22 ; Skip the egghunter shellcode
 _huntloop:
     
-    inc rdi
-    cmp dword [rdi], the_egg
+    inc rsp
+    cmp dword [rsp], the_egg
     jne _huntloop
-    add rdi, 4
-    jmp rdi
-
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-db 0x41
-db 0x41
-db 0x41
-db 0x41
-push rax
-push rdi
-push rsi
+    add rsp, 4
+    jmp rsp
